4bc5e196b08c1db76f6ebf9c89ceacebfe063eb5
MooniniteModz <mooninitemodz@proton.me>
2025-07-19 14:50:55 -0500

    Logo Update

--- TradeNetics.WebApp/obj/Debug/net8.0/TradeNetics.WebApp.AssemblyInfo.cs
+++ TradeNetics.WebApp/obj/Debug/net8.0/TradeNetics.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradeNetics.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f41206e17a5b682dff8ee99f6ae7394a3701b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ce7d1dd5dfb0a9a8fc79a90de57c71cc45c8f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradeNetics.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradeNetics.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
